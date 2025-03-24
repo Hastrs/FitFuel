1a10f1e326f3209977cb0b23f377136c172d0b60
Hassan <hastr2021@gmail.com>
2025-03-24 22:14:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// Initialize AOS animations
-AOS.init({
-  once: true,
-  offset: 100
-});
-    
+
     // Mobile Menu Toggle
     const menuToggle = document.getElementById('menuToggle');
     const navLinks = document.getElementById('navLinks');
